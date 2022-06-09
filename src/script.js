ac85720bff3b59518288b45ba0f682496f61a9b2
carinalsilverio <carinalsilverio@gmail.com>
2022-06-08 21:50:41 -0300

    Retirada frase automática Whatsapp, adição de ícone no título, funcionamento formulário

--- src/script.js
+++ src/script.js
@@ -2,20 +2,18 @@
 
 const menuItems = document.querySelectorAll('.nav-link');
 
-menuItems.forEach(item => {
-    item.addEventListener ('click', scrollToIdOnClick);
+menuItems.forEach((item) => {
+  item.addEventListener('click', scrollToIdOnClick);
 });
 
 function scrollToIdOnClick(event) {
-    event.preventDefault(); /*retirar evento padrão*/
-    const element = event.target;
-    const id = element.getAttribute('href');
-    const to = document.querySelector(id).offsetTop;
-    
-    window.scroll ({
-        top: to,
-        behavior: "smooth",
-    })
+  event.preventDefault(); /*retirar evento padrão*/
+  const element = event.target;
+  const id = element.getAttribute('href');
+  const to = document.querySelector(id).offsetTop;
+
+  window.scroll({
+    top: to,
+    behavior: 'smooth',
+  });
 }
-
-
