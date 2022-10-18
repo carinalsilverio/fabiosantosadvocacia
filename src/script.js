2d30966c3ca6cada78109a3c0577c1cfb3983ad2
carinalsilverio <carinalsilverio@gmail.com>
2022-10-17 22:11:50 -0300

    Redimensionamento imagem-equipe, scroll suave botão topo

--- src/script.js
+++ src/script.js
@@ -70,11 +70,11 @@ $(window).scroll(function () {
   }
 });
 
-// Botão topo Scroll Suave
+// Botão Votlar ao Topo 
 
 const scrollAnima = document.querySelector('[data-anima=scroll]');
 
-const metadeWindow = window.innerHeight * 5.0;
+const metadeWindow = window.innerHeight * 4.0;
 
 function animarScroll() {
   const topoItem = scrollAnima.getBoundingClientRect().top;
@@ -88,19 +88,3 @@ function animarScroll() {
 
 window.addEventListener('scroll', animarScroll);
 
-/* Scroll Suave Botão-Topo */
-const linkInterno = document.querySelectorAll(
-  '[data-scroll= "suave"] a[href^="#"]',
-);
-
-function ScrollTop(event) {
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const ancora = document.querySelector(href);
-  ancora.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-  });
-}
-
-linkInterno.addEventListener('click', ScrollTop);
